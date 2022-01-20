fba481c9294979b2b0588c1050b5cf9afb189abf
bugaga92 <90802877+bugaga92@users.noreply.github.com>
2022-01-20 14:30:41 +0300

    5.7-5.8 1-я попытка.

--- source/js/menutoggle.js
+++ source/js/menutoggle.js
@@ -1,10 +1,12 @@
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
+navMain.classList.remove('main-nav--nojs');
+
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav__open')) {
-    navMain.classList.remove('main-nav__open');
+  if (navMain.classList.contains('main-nav__opened')) {
+    navMain.classList.remove('main-nav__opened');
   } else {
-    navMain.classList.add('main-nav__open');
+    navMain.classList.add('main-nav__opened');
   }
 });
